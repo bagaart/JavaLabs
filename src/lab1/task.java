ccd18558ca8406e3f0c1217ac346db19e8cd03ae
bagaart <artemka.bagautdinoff1@yandex.ru>
2025-02-10 09:49:20 +0300

    1/9

---
+++ src/lab1/task.java
@@ -0,0 +1,34 @@
+package lab1;
+
+public class task {
+    public static void main(String[] args){
+        if (args.length == 0) {
+            System.out.println("Не введена последовательность целых чисел.");
+            return;
+        }
+
+        int a = 0;
+        int b = 10;
+
+        int[] numbers = new int[args.length];
+        for (int i = 0; i < args.length; i++){
+            try {
+                numbers[i] = Integer.parseInt(args[i]);
+            } catch (NumberFormatException e) {
+                System.out.println("Аргумент " + args[i] + " не целое число.");
+                return;
+            }
+        }
+        int flag = 0;
+        for (int number : numbers) {
+            if (number < b && number > a) {
+                System.out.println(number);
+                flag = 1;
+            }
+        }
+
+        if (flag == 0) {
+            System.out.printf("Чисел принадлежащих отрезку[%d, %d] нет.", a, b);
+        }
+    }
+}
